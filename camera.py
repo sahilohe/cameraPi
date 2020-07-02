5c4f4ad79e35ef1f326a4fd3ca0f35aa0d8e3693
Sahil Ohe <sahilohe97@gmail.com>
2020-07-02 07:59:17 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -9,14 +9,14 @@ camera = PiCamera()#camera started
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be off)
 
 
 while True: # run the code forever
-    camera.start_preview()#gets a preview on your screen
+    camera.start_preview() #gets a preview on your screen forever
     if GPIO.input(10) == GPIO.HIGH : #enter your GPIO button pin number
         print("Clicking photo")
-        os.system("raspistill -o /home/pi/Desktop/image%s.jpg") #takes a photo and saves to Desktop
+        os.system("raspistill -o /home/pi/Desktop/image%s.jpg") #takes a photo and saves to Desktop (change the directory if you want)
         sleep(2)
         camera.stop_preview()#stops the screen preview
 
