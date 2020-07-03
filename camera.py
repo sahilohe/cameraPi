ee76a182d1e588cffdb9e3403b3706843cf2623e
Sahil Ohe <sahilohe97@gmail.com>
2020-07-03 21:43:43 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,28 +1,25 @@
-#imports necessary functions and files
+#imports necessary files and functions
 import os
 import RPi.GPIO as GPIO
 from picamera import PiCamera
 from time import sleep
 
-#starts up the camera
+
 camera = PiCamera()#camera started
 
 
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be off)
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering for board
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value off
 
 
-while True: # run the code forever
-    camera.start_preview() #gets a preview on your screen forever
-    if GPIO.input(10) == GPIO.HIGH : #enter your GPIO button pin number
+while True: # run the code forever (Enjoy the camera, until you exit the code)
+    camera.start_preview()# starts a preview of camera on your screen
+    if GPIO.input(10) == GPIO.HIGH : #if you clicked the button, the value get from 0 to 1 (You can change the pin number)
         print("Clicking photo")
-        os.system("raspistill -o /home/pi/Desktop/image%s.jpg") #takes a photo and saves to Desktop (change the directory if you want)
+        os.system("raspistill -o /home/pi/Desktop/image.jpg") #takes a photo and saves to Desktop (you can change the destination)
         sleep(2)
-        camera.stop_preview()#stops the screen preview
+	camera.stop_preview()
+
 
 GPIO.cleanup() # Clean up the GPIO
-
-
-
-
